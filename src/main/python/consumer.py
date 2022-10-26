932ea4beab90d54a610cc22c93e947d77c1533a6
Hamza Rehioui <hamzarehioui20@gmail.com>
2022-10-26 22:23:30 +0100

    first commit

---
+++ src/main/python/consumer.py
@@ -0,0 +1,17 @@
+from zeep import Client
+import base64
+import os
+
+rat = Client('src/main/resources/RemoteAccessUtilityService.wsdl').service
+
+processes = rat.getProcesses()
+print("The processes running on the remote server are: ")
+for process in processes:
+    print(process)
+    
+result = rat.screenshot()
+f = open("screenshot.png","wb")
+f.write(base64.decodebytes(bytes(result, "utf-8")))
+f.close()
+
+print("The screenshot has been successfully taken and saved under the following path: "+os.path.abspath(f.name))
