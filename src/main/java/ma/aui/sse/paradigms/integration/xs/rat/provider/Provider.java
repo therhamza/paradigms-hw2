cda3835c96dd1fc652f2834fee178cdd8ca2aed2
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-20 18:30:23 +0100

    Update Provider.java

--- src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/Provider.java
+++ src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/Provider.java
@@ -2,16 +2,30 @@ package ma.aui.sse.paradigms.integration.xs.rat.provider;
 
 import javax.xml.ws.Endpoint;
 
+/**
+ * The Provider class is a simple Java application that publishes an instance
+ * of the RemoteAccessUtility class as a web service at the URL
+ * "http://localhost:8080/rat".
+ */
 public class Provider {
 
+    // The URL at which the web service will be published
     private static final String URL = "http://localhost:8080/rat";
 
     public static void main(String[] args) {
+        // Set the java.awt.headless system property to false
         System.setProperty("java.awt.headless", "false");
+        
+        // Create an instance of the RemoteAccessUtility class
         RemoteAccessUtility remoteAccessUtility = new RemoteAccessUtility();
+        
+        // Print a message indicating that the service is being published
         System.out.println("Publishing RemoteAccessUtility Service");
+        
+        // Publish the web service
         Endpoint.publish(URL, remoteAccessUtility);
+        
+        // Print a message indicating that the service has been published
         System.out.println("RemoteAccessUtility Service Published");
     }
-    
 }
