dc41fdc586ff13ca6a5ae8fa5a140eee0e35375b
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-20 18:35:47 +0100

    Update RemoteAccessUtility.java

--- src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/RemoteAccessUtility.java
+++ src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/RemoteAccessUtility.java
@@ -14,79 +14,66 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.Robot;
 
+/**
+ * This class provides a web service that allows remote access to various system-level functions on a
+ * computer running the Java Virtual Machine (JVM).
+ */
 @WebService
 public class RemoteAccessUtility {
 
+    /**
+     * Returns a list of all running processes on the system, along with some details about each process.
+     *
+     * @return an ArrayList of strings containing details about each process
+     */
     public ArrayList<String> getProcesses() {
-
         ArrayList<String> listOfAllProcesses = new ArrayList<String>();
-            
-        StringBuilder processes = new StringBuilder("");
-
         ProcessHandle.allProcesses().forEach(
             process -> listOfAllProcesses.add(processDetails(process)) 
         );
-
         return listOfAllProcesses;
-
     }
 
+    /**
+     * Takes a screenshot of the entire screen and returns the screenshot as a base64-encoded string.
+     *
+     * @return a base64-encoded string representing the screenshot
+     */
     public String screenshot() {
-
         try {
-
             Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
             BufferedImage screenshot = new Robot().createScreenCapture(screenRect);
-
             String extension = "png";
-
             return encodeToString(screenshot, extension);
-
-        } 
-        catch (Exception ex) {
-            
+        } catch (Exception ex) {
             return "";
-
         }
     }
 
+    /**
+     * Reboots the system.
+     *
+     * @return true if the reboot was successful, false otherwise
+     */
     public boolean reboot() {
-
         try {
-
             ProcessBuilder builder = System.getProperty("os.name").toLowerCase().contains("win")
                     ? new ProcessBuilder("shutdown", "-r", "-t", "1")
                     : new ProcessBuilder("shutdown", "-r", "now");
-
             Process process = builder.start();
-
             return true;
-
-        } 
-        catch (Exception ex) {
-            
+        } catch (Exception ex) {
             return false;
-
         }
-							
     }
 
-    private static String processDetails(ProcessHandle process) {
-
-		return String.format("%8d %8s %10s %-40s",
-				process.pid(),
-				text(process.parent().map(ProcessHandle::pid)),
-				text(process.info().user()),
-                text(process.info().commandLine()));
-                
-	}
-	
-    private static String text(Optional<?> optional) {
-
-        return optional.map(Object::toString).orElse("-");
-
-    }
-
+    /**
+     * Encodes a BufferedImage object as a base64-encoded string.
+     *
+     * @param image the BufferedImage object to be encoded
+     * @param type the file type of the image (e.g. "png")
+     * @return a base64-encoded string representing the image
+     */
     private static String encodeToString(BufferedImage image, String type) {
          
         String imageString = null;  
